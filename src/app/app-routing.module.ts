907040f8e5919bf69580939928ffcc3e57c8c08e
Santi <sestevez5@gmail.com>
2020-08-06 17:13:16 +0100

    Primer commit template listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EstadisticaComponent } from './components/ingreso-egreso/estadistica/estadistica.component';
+import { IngresoEgresoComponent } from './components/ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from './components/ingreso-egreso/detalle/detalle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '',
+    component: DashboardComponent,
+    children: [
+      { path: '', component: EstadisticaComponent },
+      { path: 'ingreso-egreso', component: IngresoEgresoComponent },
+      { path: 'detalle', component: DetalleComponent }
+    ]
+  },
+  { path: '**', redirectTo: ''}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Modules
 import { AppRoutingModule } from './app-routing.module';
+
+//Components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './components/ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './components/ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './components/ingreso-egreso/detalle/detalle.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AppRoutingModule
   ],
   providers: [],
